62d55965b8b3d930271135e59c6e89bbd60001fb
dianhenglau <dianhenglau@gmail.com>
2018-05-25 17:44:35 +0800

    Add password and comments.

--- database/init_db.sql
+++ database/init_db.sql
@@ -1,8 +1,13 @@
 -- This script create database and role. It should be run by user (e.g.
--- `postgres`) that has required permissions.
+-- `postgres`) that has permissions to create role and create database.
 
-CREATE ROLE leev_admin LOGIN;
-CREATE ROLE leev_user LOGIN;
+-- This role is created to manage the `leev` database, e.g. create tables.
+-- You can use this role to run `psql`. REMEMBER TO CHANGE THE PASSWORD!
+CREATE ROLE leev_admin LOGIN PASSWORD 'foo';
+
+-- This role is created to use the `leev` database, e.q. query. REMEMBER TO
+-- CHANGE THE PASSWORD!
+CREATE ROLE leev_user LOGIN PASSWORD 'bar';
 
 CREATE DATABASE leev OWNER leev_admin;
 
